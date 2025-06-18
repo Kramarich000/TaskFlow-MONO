1585847c69c7c4e749bd3a02b489d91cbfa06e38
Karen <karen.avakov2@gmail.com>
2025-06-18 13:42:20 +0300

    убрал лишнюю штуку

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,6 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
 });
